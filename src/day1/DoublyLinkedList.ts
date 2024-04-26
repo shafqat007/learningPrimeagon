d25b518cfaca1d37187ec12c83a7126d14a78eec
shafqat007 <shafqatnawazchy@gmail.com>
2024-04-26 10:48:17 +0600

    Frontend

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -25,3 +25,9 @@ export default class DoublyLinkedList<T> {
 
 }
 }
+//ekta single linked list arekta double linked list, double linked list e ekta node er 2 ta pointer thake, ekta next ar ekta previous
+//linked list e O(1) insert, delete/remove,prepend/append(append: Add something to the end. prepend: Add something to the beginning. ) getting head/tail(karon we hve defined pointer to that,same bhabe delettion from heador tail constanr) (eta hocche constant operation,not variable:variable hoile
+// input er upor nirbhor //deletion in the middle hoile duita operation
+//) ,input er upor nirbhor kore nah, 
+//mane linked list kotoboro doesnt matter 
+//linked list e index nai,shb gula hoitese node, array e index thake, array e index diye access kora jay, linked list e na
--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,11 +1,13 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
-
-
-    for (let i = 0; i < haystack.length; ++i) {
-        if (haystack[i] === needle) {
-            return true
+export default function linear_search(numbers: number[], target: number): boolean {
+    for (let i = 0; i < numbers.length; ++i) {
+        if (numbers[i] === target) {
+            return true;
         }
     }
     return false;
 }
+
+// Example usage:
+const numbersArray = [4, 7, 2, 8, 3];
+const targetNumber = 8;
+console.log(linear_search(numbersArray, targetNumber)); // Output: true
